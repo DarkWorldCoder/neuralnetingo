6856f8a66730f081b141ea9f07f681c66ef83681
Ayush Niroula <ayushniroula08@gmail.com>
2025-11-02 17:42:48 +0545

    Go num addidiotn

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/DarkWorldCoder/neuralnetingo
+
+go 1.25.3
+
+require gonum.org/v1/gonum v0.16.0 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"gonum.org/v1/gonum/mat"
+)
 
 type Neuron struct {
 	weights []float64
 	bias    float64
 }
 
-func main() {
-	inputs := [4]float64{1, 2, 3, 2.5}
+func DotProduct(a, b []float64) float64 {
+	if len(a) != len(b) {
+		panic("Vectors must be of the same length")
+	}
+	var result float64
+	for i := range a {
+		result += a[i] * b[i]
+	}
+	return result
+}
+
+func RunModel() {
+	inputs := []float64{1, 2, 3, 2.5}
 
 	n1 := Neuron{
 		weights: []float64{0.2, 0.8, -0.5, 1.0},
@@ -26,12 +41,19 @@ func main() {
 	var output []float64
 	for _, neuron := range neurons {
 		var neuron_output float64
-		for i, weight := range neuron.weights {
-			neuron_output += inputs[i] * weight
-		}
-		neuron_output += neuron.bias
+		neuron_output = DotProduct(neuron.weights, inputs[:]) + neuron.bias
 		output = append(output, neuron_output)
 	}
 	fmt.Println("Output:", output)
-	// fmt.Println(inputs, weights)
+}
+func main() {
+	a := mat.NewVecDense(3, []float64{1, 2, 3})
+	b := mat.NewVecDense(3, []float64{2, 3, 4}).T()
+
+	result := mat.NewDense(1, 1, nil)
+	result.Mul(b, a)
+
+	fmt.Printf("Result:\n%v\n", mat.Formatted(result))
+	// RunModel()
+
 }
