3f5e728056a34b87efff8f97bf6521afb04912e4
Ayush Niroula <ayushniroula08@gmail.com>
2025-11-03 19:07:51 +0545

    Multi layer neuron calculation

--- main.go
+++ main.go
@@ -11,20 +11,8 @@ type Neuron struct {
 	bias    float64
 }
 
-func DotProduct(a, b []float64) float64 {
-	if len(a) != len(b) {
-		panic("Vectors must be of the same length")
-	}
-	var result float64
-	for i := range a {
-		result += a[i] * b[i]
-	}
-	return result
-}
-
 func RunModel() {
-	inputs := []float64{1, 2, 3, 2.5}
-
+	inputs := [][]float64{{1, 2, 3, 2.5}, {2.0, 5.0, -1.0, 2.0}, {-1.5, 2.7, 3.3, -0.8}}
 	n1 := Neuron{
 		weights: []float64{0.2, 0.8, -0.5, 1.0},
 		bias:    2.0,
@@ -37,23 +25,40 @@ func RunModel() {
 		weights: []float64{-0.26, -0.27, 0.17, 0.87},
 		bias:    0.5,
 	}
-	neurons := []Neuron{n1, n2, n3}
-	var output []float64
-	for _, neuron := range neurons {
-		var neuron_output float64
-		neuron_output = DotProduct(neuron.weights, inputs[:]) + neuron.bias
-		output = append(output, neuron_output)
+	weights := [][]float64{
+		n1.weights,
+		n2.weights,
+		n3.weights,
 	}
-	fmt.Println("Output:", output)
+	biases := []float64{n1.bias, n2.bias, n3.bias}
+
+	input_dense := mat.NewDense(len(inputs), len(inputs[0]), nil)
+	for i := 0; i < len(inputs); i++ {
+		for j := 0; j < len(inputs[0]); j++ {
+			input_dense.Set(i, j, inputs[i][j])
+		}
+	}
+
+	weight_dense := mat.NewDense(len(weights), len(weights[0]), nil)
+	for i := 0; i < len(weights); i++ {
+		for j := 0; j < len(weights[0]); j++ {
+			weight_dense.Set(i, j, weights[i][j])
+		}
+	}
+
+	var output_dense mat.Dense
+	output_dense.Mul(input_dense, weight_dense.T())
+
+	for i := 0; i < output_dense.RawMatrix().Rows; i++ {
+		for j := 0; j < output_dense.RawMatrix().Cols; j++ {
+			output_dense.Set(i, j, output_dense.At(i, j)+biases[j])
+		}
+	}
+
+	fmt.Printf("Output Dense:\n%v\n", mat.Formatted(&output_dense))
 }
 func main() {
-	a := mat.NewVecDense(3, []float64{1, 2, 3})
-	b := mat.NewVecDense(3, []float64{2, 3, 4}).T()
 
-	result := mat.NewDense(1, 1, nil)
-	result.Mul(b, a)
-
-	fmt.Printf("Result:\n%v\n", mat.Formatted(result))
-	// RunModel()
+	RunModel()
 
 }
